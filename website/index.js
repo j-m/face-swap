2ab6ce78d2804a89c571952d0f58556393541115
Jonathan Marsh <j.r.marsh@ntlworld.com>
2017-11-19 10:31:22 +0000

    Form submission corrected

--- website/index.js
+++ website/index.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
-	$('#submit').click(function(event){
-		var url = "";
-		$('#result').css('display', 'block');
-		$.getJSON(url, function(data) {
-			$('#result').src(data.src);
-		}).fail(function(jqXHR, textStatus, errorThrown) { $("#error").show(); });
+	var busy = false;
+	$('form').submit(function( event ) {
+		event.preventDefault();
+		if(busy==false){
+			busy = true;
+			var url = "";
+			$('#result').css('display', 'block');
+			var send = {father:$('#father').val(), mother:$('#mother').val()};
+			$.getJSON(url, send)
+			.done(function(data) {
+				$('#result').src(data.src);
+				busy = false;
+			})
+			.fail(function(jqXHR, textStatus, errorThrown) { $("#error").show(); });
+		}
 	});
 });
