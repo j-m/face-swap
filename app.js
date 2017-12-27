bb8e7842ad29b49b7d82c4f11b21a40f233f6e6a
Jonathan Marsh <j.r.marsh@ntlworld.com>
2017-12-27 23:07:39 +0000

    Removed irrelevant ported code

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
+require('dotenv-safe').load();
 var listenOnPort = 8084,
+    combinations = require('./combinations.json'),
     debug = require('debug'),
     path = require('path'),
     favicon = require('serve-favicon'),
@@ -27,90 +29,31 @@ if (app.get('env') === 'development') {
         });
     });
 }
-app.get('/stats', function (req, res) {
+app.get('/combine', function (req, res) {
     function callback() {
         res.json(result);
     }
-    if (req.query.url) {
-        var uri = url.parse(decodeURIComponent(req.query.url)),
-            result = {},
-            waiting = 4,
-            options = {
-                app_api_key: process.env.API_KEY,
-                cmd: "GetIndexItemInfo",
-                items: 1,
-                item0: uri.href,
-                datasource: "fresh"
-            };
-        request({ url: `http://developer.majestic.com/api/json`, qs: options }, function (err, res, body) {
-            if (err)
-                console.log("Get request failed: " + err);
-            if (res.statusCode === 200)
-                try {
-                    data = JSON.parse(body);
-                    if (data['Code'] !== 'OK')
-                        console.log("Majestic returned non-OK status");
-                    else {
-                        var majestic_data = data['DataTables']['Results']['Data'][0];
-                        result['CitationFlow'] = majestic_data['CitationFlow'];
-                        result['TrustFlow'] = majestic_data['TrustFlow'];
-                        result['Topic'] = majestic_data['TopicalTrustFlow_Topic_0'];
-                        result['TopicValue'] = majestic_data['TopicalTrustFlow_Value_0'];
+    if (req.query.father && req.query.mother) {
+        var waiting = 1,
+            father = req.query.father,
+            mother = req.query.mother;
+        if (votes[father + " and " + mother]) {
+            res.json({ statusCode: 200, data: votes[father + " and " + mother] });
+        } else {
+            https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&pithumbsize=500&titles=Jennifer+Lawrence
+            request({ method: 'GET', url: uri.href }, function (err, res, body) {
+                if (err)
+                    console.log("Get request failed: " + err);
+                if (res.statusCode === 200) {
+                    var article = unfluff(body, 'en');
+                    if (article) {
+                        var title = article.softTitle, words = article.text.split(" ");
                     }
-                } catch (e) {
-                    console.log("Could not parse into JSON: " + body);
-                }
-            if (--waiting === 0) callback();
-        });
-
-        request({ method: 'GET', url: uri.href }, function (err, res, body) {
-            if (err)
-                console.log("Get request failed: " + err);
-            if (res.statusCode === 200) {
-                var article = unfluff(body, 'en');
-                if (article) {
-                    var title = article.softTitle, words = article.text.split(" "), count = {};
-                    for (var word in words)
-                        count[word]++;
-                    result["WordCountCoeff"] = Object.keys(count).length / 10;
-                    if (result["WordCountCoeff"] > 25) result["WordCountCoeff"] = 25;
-                    result['Betteridge'] = title.substring(title.length - 1) !== "?";
-                    wordpos.getPOS(article.text, function (res) {
-                        result['Grammar'] = { 'Nouns': res.nouns.length, 'Verbs': res.verbs.length, 'Adjectives': res.adjectives.length, 'Adverbs': res.adverbs.length, 'Other': res.rest.length };
-                        if (--waiting === 0) callback();
-                    });
-
-                    emotional.load(function () {
-                        var text = emotional.get(article.text);
-                        result['Polarity'] = text.polarity;
-                        result['Subjectivity'] = text.subjectivity;
-                        if (--waiting === 0) callback();
-                    });
-                }
-            }
-            if (--waiting === 0) callback();
-        });
-
-        var domain = uri.hostname.replace(/^www\./, '');
-        if (votes[domain]) {
-            result['VotesFor'] = votes[domain].y;
-            result['VotesAgainst'] = votes[domain].n;
-        }
-    } else res.json({ Error:"URI Parameter missing"});
-});
-app.get('/vote', function (req, res) { 
-    if (req.query.trusted && req.query.url) {
-        if (req.query.trusted === "y" || req.query.trusted === "n") {
-            var uri = url.parse(decodeURIComponent(req.query.url)).hostname.replace(/^www\./, '');
-            console.log(req.query.trusted, " vote for ", uri);
-            if (!votes[uri]) votes[uri] = { "y": 0, "n": 0 };
-            votes[uri][req.query.trusted]++;
-            res.json(votes[uri]);
-            fs.writeFile("./votes.json", JSON.stringify(votes), "utf8", function (err) {
-                if (err) console.log(err);
+                } else { console.log("GET request failed, error code: " + res.statusCode); }
+                if (--waiting === 0) callback();
             });
         }
-    } else res.json({ Error: "Parameter missing" });
+    } else res.json({ statusCode: 400, data:"Bad Request: URI Parameter missing"});
 });
 app.get('/', function (req, res) { res.sendFile(__dirname + '/public/index.html'); });
 app.get('*', function (req, res) { res.sendFile(__dirname + '/public/error.html'); });
